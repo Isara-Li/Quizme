36c5eb4a3ce6d2455c434bad542f5b90edffa849
Isara-Li <vimandika.21@cse.mrt.ac.lk>
2024-12-07 22:46:20 +0530

    Basic changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,11 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import {Inter} from "next/font/google";
+import { cn } from "@/lib/utils";
+import Navbar from "@/components/Navbar";
+
+const inter = Inter({ subsets: ["latin"] });
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -20,15 +25,14 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={cn(inter.className, "antialiased min-h-screen pt-16")}>
+        <Navbar />
+          {children}
       </body>
     </html>
   );
