531df842f63d83b334ed323b809da482ec197f88
Isara-Li <vimandika.21@cse.mrt.ac.lk>
2025-02-04 23:27:52 +0530

    Add the mcq game page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import {Inter} from "next/font/google";
 import { cn } from "@/lib/utils";
 import Navbar from "@/components/Navbar";
 import Providers from "@/components/Providers";
+import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -43,6 +44,7 @@ export default function RootLayout({
           <main className="flex-grow pt-16">
             {children}
           </main>
+          <Toaster />
         </Providers>
       </body>
     </html>
